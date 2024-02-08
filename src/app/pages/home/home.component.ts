146fe14c4b923f65f7b4b8c4750dbb6b87b9091e
E1230 <emaurobel1230@hotmail.com>
2024-02-07 21:45:02 -0500

    Clase 10

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,4 +16,16 @@ export class HomeComponent {
     'Ver Angular',
     'Ver flutter',
   ]);
+
+  changeHandler(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newTask = input.value;
+    this.tasks.update((tasks) => [...tasks, newTask]);
+  }
+
+  deleteTask(index: number) {
+    this.tasks.update((tasks) =>
+      tasks.filter((task, position) => position !== index)
+    );
+  }
 }
